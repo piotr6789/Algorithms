52fddcd7dd017d3251d22b0cbabdf000eb245189
Piotr Ulanicki <piotr.ulanicki4@gmail.com>
2019-03-14 02:36:56 +0100

    all work

--- Algorithms/Algorithms/ChangeMaking.cs
+++ Algorithms/Algorithms/ChangeMaking.cs
@@ -12,12 +12,12 @@ namespace Algorithms
         public static string summary = "";
         public static double amount = 0;
         public static int[] coins = { 1, 2, 5, 10, 20, 50, 100, 200, 500, 1000, 2000, 5000, 10000, 20000 };
-        public static int[] limits = { 123, 55, 54, 32, 12, 34, 22, 11, 9, 8, 7, 6, 5, 4 };
-        public static bool hasKey = true;
+        public static int[] limits = { 2, 32, 23, 32, 53, 32, 33, 21, 22, 15, 12, 10, 5, 4 };
+        public static bool hasCoin = true;
 
-        public static void Start()
+        public static void Dynamic()
         {
-            while (hasKey)
+            while (hasCoin)
             {
                 summary = "";
                 Random randomAmount = new Random();
@@ -25,7 +25,10 @@ namespace Algorithms
                 //amount = double.Parse(Console.ReadLine());
                 Console.WriteLine("Cena wynosi: {0}zł", payment);
                 Console.Write("Podaj kwotę: ");
-                amount = (double.Parse(Console.ReadLine()) - payment) * 100;
+                double check = double.Parse(Console.ReadLine());
+                check = check * 100;
+                payment = payment * 100;
+                amount = (check - payment);
 
 
                 int[][] coinsUsed = new int[(int)amount + 1][];
@@ -72,77 +75,91 @@ namespace Algorithms
                 }
                 for (int i = 0; i < limits.Length; i++)
                 {
-                    while (coinsUsed[(int)amount][i] >= 0)
+                    while (coinsUsed[(int)amount][i] > 0)
                     {
                         if (i == 0)
                         {
                             summary = summary + "0.01" + "zł ";
                             coinsUsed[(int)amount][i]--;
+                            limits[i]--;
                         }
                         if (i == 1)
                         {
                             summary = summary + "0.02" + "zł ";
                             coinsUsed[(int)amount][i]--;
+                            limits[i]--;
                         }
                         if (i == 2)
                         {
                             summary = summary + "0.05" + "zł ";
                             coinsUsed[(int)amount][i]--;
+                            limits[i]--;
                         }
                         if (i == 3)
                         {
                             summary = summary + "0.1" + "zł ";
                             coinsUsed[(int)amount][i]--;
+                            limits[i]--;
                         }
                         if (i == 4)
                         {
                             summary = summary + "0.2" + "zł ";
                             coinsUsed[(int)amount][i]--;
+                            limits[i]--;
                         }
                         if (i == 5)
                         {
                             summary = summary + "0.5" + "zł ";
                             coinsUsed[(int)amount][i]--;
+                            limits[i]--;
                         }
                         if (i == 6)
                         {
                             summary = summary + "1" + "zł ";
                             coinsUsed[(int)amount][i]--;
+                            limits[i]--;
                         }
                         if (i == 7)
                         {
                             summary = summary + "2" + "zł ";
                             coinsUsed[(int)amount][i]--;
+                            limits[i]--;
                         }
                         if (i == 8)
                         {
                             summary = summary + "5" + "zł ";
                             coinsUsed[(int)amount][i]--;
+                            limits[i]--;
                         }
                         if (i == 9)
                         {
                             summary = summary + "10" + "zł ";
                             coinsUsed[(int)amount][i]--;
+                            limits[i]--;
                         }
                         if (i == 10)
                         {
                             summary = summary + "20" + "zł ";
                             coinsUsed[(int)amount][i]--;
+                            limits[i]--;
                         }
                         if (i == 11)
                         {
                             summary = summary + "50" + "zł ";
                             coinsUsed[(int)amount][i]--;
+                            limits[i]--;
                         }
                         if (i == 12)
                         {
                             summary = summary + "100" + "zł ";
                             coinsUsed[(int)amount][i]--;
+                            limits[i]--;
                         }
                         if (i == 13)
                         {
                             summary = summary + "200" + "zł ";
                             coinsUsed[(int)amount][i]--;
+                            limits[i]--;
                         }
                     }
                 }
--- Algorithms/Algorithms/Program.cs
+++ Algorithms/Algorithms/Program.cs
@@ -10,7 +10,7 @@ namespace Algorithms
     {
         static void Main(string[] args)
         {
-            ChangeMaking.Start();
+            ChangeMaking.Dynamic();
         }
     }
 }
